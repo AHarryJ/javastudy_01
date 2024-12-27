d9cb91fa2bae1efa1f0e9aaface8bd700dbc9488
harry AN <anhyunjun8721@gmail.com>
2024-12-27 16:05:38 +0900

    Initial commit

---
+++ src/java_study_01.java
@@ -0,0 +1,10 @@
+public class java_study_01 {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5};
+        for(int i = 0; i < num.length; i++){
+            if(num[i] % 2 == 0){
+                System.out.println(num[i]);
+            }
+        }
+    }
+}
